5ab0499820903f8f29cd8ad4ce60033813e7c761
mark <manhng132@gmail.com>
2024-08-27 23:33:34 +0700

    Update SignInButton.tsx

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -17,12 +17,6 @@ const SignInButton = ({ textSignIn, textSignUp }: Props) => {
       >
         {textSignIn}
       </Button>
-      <Button
-        className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition duration-300"
-        onClick={() => router.push("/signup")}
-      >
-        {textSignUp}
-      </Button>
     </div>
   );
 };
